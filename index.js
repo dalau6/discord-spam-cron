7e951b89e89c5ef7cd0e9b26afb7856a2357bcd5
David <dalau6@gmail.com>
2021-03-16 22:16:47 -0700

    Refactored to level up

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const puppeteer = require('puppeteer');
-const fetch = require('node-fetch');
-const { id, password } = require('./auth.json');
-
+const puppeteer = require('puppeteer'),
+  { id, password } = require('./auth.json');
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://discord.com/login');
-  await page.type('.inputDefault-_djjkz[name=email]', id);
-  await page.type('.inputDefault-_djjkz[name=password]', password);
-  await page.click('button.marginBottom8-AtZOdT');
-  await page.waitForNavigation();
-  await page.goto('https://discord.com/channels/798398605614514196/798398605907460100');
-  
-  setInterval(async () => {
-    const response = await fetch('https://api.whatdoestrumpthink.com/api/v1/quotes/random');
-    const { message } = await response.json();
-    await page.focus('[data-slate-zero-width=z]');
-    await page.type('[data-slate-zero-width=z]', message);
-    await page.keyboard.press('Enter');
-  }, 1000);
+  const a = await puppeteer.launch(),
+    e = await a.newPage();
+  await e.goto('https://discord.com/login'),
+    await e.type('.inputDefault-_djjkz[name=email]', id),
+    await e.type('.inputDefault-_djjkz[name=password]', password),
+    await e.click('button.marginBottom8-AtZOdT'),
+    await e.waitForNavigation(),
+    await e.goto(
+      'https://discord.com/channels/798398605614514196/798398606377484298'
+    ),
+    setInterval(async () => {
+      console.debug(new Date());
+      await e.focus('[data-slate-zero-width=z]'),
+        await e.type('[data-slate-zero-width=z]', '.'),
+        await e.keyboard.press('Enter'),
+        await e.keyboard.press('ArrowUp', { delay: 1e3 }),
+        await e.keyboard.press('Backspace', { delay: 1e3 }),
+        await e.keyboard.press('Enter', { delay: 1e3 }),
+        await e.keyboard.press('Enter', { delay: 1e3 });
+    }, 6e4);
 })();
